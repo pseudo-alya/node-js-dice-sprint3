7295f47d8b76e9d170b853cb32b930374d4d5ef1
Alya AlDiqqi <alya.ad233@gmail.com>
2025-03-01 22:57:15 +0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files (HTML, CSS, images, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// API endpoint for rolling the dice
+app.get('/roll-dice', (req, res) => {
+    const diceRoll = Math.floor(Math.random() * 6) + 1;
+    res.json({ diceRoll });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
